1e2727d4fcbabe51860cc22a810db2435a3ceb71
源稚生 <yzhisheng94666@gmail.com>
2025-05-23 19:25:41 +0800

    props配置

--- Vue2/vue2_test/vue.config.js
+++ Vue2/vue2_test/vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false, //关闭语法检查
+  transpileDependencies: true
 })
